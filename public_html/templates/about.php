a4d6f11446294bc359098e1def2db150d094a66d
amy skidmore <askidmore1@cnm.edu>
2017-06-08 11:58:45 -0600

    added template/route/component

--- public_html/templates/about.php
+++ public_html/templates/about.php
@@ -5,3 +5,15 @@
  * Date: 6/8/17
  * Time: 11:32 AM
  */
+<main class="bg p-t-nav">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-4">
+				<div class="jumbotron text-right">
+					<h1>Welcome to the Meow App!</h1>
+					<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {AboutComponent} from "./components/about.component";
-
+import {PostsComponent} from "./components/posts.component";
 
 export const allAppComponents = [HomeComponent, AboutComponent];
 
 export const routes: Routes = [
+	{path: "posts", component: PostsComponent},
 	{path: "about", component: AboutComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
